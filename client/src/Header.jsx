855b251c355d0f8b7eecb47c05fc8583e2dffb29
JaiminR7 <173jaiminradia@gmail.com>
2025-02-22 01:44:01 +0530

    first commit

---
+++ client/src/Header.jsx
@@ -0,0 +1,47 @@
+import { Link } from 'react-router-dom';
+export default function Header()
+{
+    return (
+        <header className=" flex justify-between">
+        <a href="" className="flex items-center gap-1">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 -rotate-90">
+           <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
+        </svg>
+
+
+          <span className="font-bold text-xl">airbnb</span>
+        </a>
+
+        {/* Search bar */}
+        <div className="flex border border-gray-300 rounded-full py-2 px-4 shadow-sm hover:shadow-md transition-shadow duration-150">
+          <div className="px-3">Anywhere</div>
+          <div className="border-l border-gray-300 h-full mx-2"></div> {/* Divider */}
+          <div className="px-3">Any Week</div>
+          <div className="border-l border-gray-300 h-full mx-2"></div> {/* Divider */}
+          <div className="px-3">Add Guests</div>
+          <button className="bg-red-500 text-white p-2 rounded-full ml-2">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+            </svg>
+          </button>
+        </div>
+
+        
+        <Link to={'/login'} className="flex border items-center gap-4 border-gray-300 rounded-full py-1 px-2 shadow-sm hover:shadow-md transition-shadow duration-150">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+</svg>
+
+   <div className="">
+   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+</svg>
+
+   </div>
+
+
+     
+        </Link>
+      </header>
+    )
+}
